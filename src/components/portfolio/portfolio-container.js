850cd67e35b453aeac716c1b55ef5bc42098f2b2
Anthonyg6 <anthonygallegos6@gmail.com>
2019-03-31 11:01:06 -0600

    Added first conditional example

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import PortfolioItem from './portfolio-items';
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            pageTitle: "Welcome to my page",
+            isLoading: false,
+            data: [
+                {"title": "ESM", category: "eCommerce Fulfillment & Distribution"},
+                {"title": "UPS", category: "eCommerce Fulfillment & Distribution"},
+                {"title": "turo", category: "Car Rental"}
+            ]
+        };
+
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    portfolioItems() {
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"}/>// props
+        });
+    }
+
+    // using the filter function to filter over the data elements that are in the intial state we are able to group the data by its category and render/set a new state of just those items
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        })
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return <div>Is Loading...</div>
+        };
+
+        return (
+            <div>
+            <h1>{this.state.pageTitle}</h1>
+            {/* we have to use an annoymous function when using this click handler because we have to pass an argument to our handleFilter function & if we do no pass it in as annoymous the app will attempt to run all of the functions at the same time, by doing this we tell the app to not run the functions until the button is clicked.*/}
+            <button onClick={()=>this.handleFilter("eCommerce Fulfillment & Distribution")}>eCommerce</button>
+            <button onClick={()=>this.handleFilter("Car Rental")}>Car Rental</button>
+            {this.portfolioItems()}
+            </div>
+            
+        )
+    };
+}
