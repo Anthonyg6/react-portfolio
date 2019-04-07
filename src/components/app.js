cf907d92be7ba577b65751b5cc2dfa387d7bc3c2
Anthonyg6 <anthonygallegos6@gmail.com>
2019-04-06 22:24:22 -0600

    added axios library to help achieve api connection

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import NavigationContainer from './navigation/navigation-container';
@@ -11,7 +12,23 @@ import PortfolioDetail from './portfolio/portfolio-detail';
 import NoMatch from './pages/no-match';
 
 export default class App extends Component {
+  // this was all added just to test the axios library. Below is pulling a get request on how to pull live data back via an api call. We have to bind the getPortfolioItems function to the "this" so that it is able to be called within the render function and pulled into our app.
+  constructor() {
+    super();
+
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
+  }
+  getPortfolioItems() {
+      axios.get('https://anthonygallegos.devcamp.space/portfolio/portfolio_items')
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
   render() {
+    this.getPortfolioItems();
     return (
       <div className='app'>
         <Router>
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -6,7 +6,7 @@ export default function() {
     <div>
       <h2>Blog</h2>
       <div>
-        <Link to="/about">Read more about me</Link>
+        <Link to="/about-me">Read more about me</Link>
       </div>
     </div>
   );
