0ed8f93695e3961b092854b2349b786114f0eb6a
Anthonyg6 <anthonygallegos6@gmail.com>
2019-04-07 09:55:48 -0600

    passed in live api data and fixed key prop

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -10,49 +10,52 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "Welcome to my page",
             isLoading: false,
-            data: [
-                {"title": "ESM", category: "eCommerce Fulfillment & Distribution", slug: 'esm'},
-                {"title": "UPS", category: "eCommerce Fulfillment & Distribution", slug: 'ups'},
-                {"title": "turo", category: "Car Rental", slug: 'turo'}
-            ]
+            data: []
         };
         
         this.handleFilter = this.handleFilter.bind(this);
         // this was all added just to test the axios library. Below is pulling a get request on how to pull live data back via an api call. We have to bind the getPortfolioItems function to the "this" so that it is able to be called within the render function and pulled into our app.
-        this.getPortfolioItems = this.getPortfolioItems.bind(this);
+        //this.getPortfolioItems = this.getPortfolioItems.bind(this);
     }
 
-    portfolioItems() {
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug}/>// props
+     // using the filter function to filter over the data elements that are in the intial state we are able to group the data by its category and render/set a new state of just those items
+     handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
         });
     }
 
     getPortfolioItems() {
         axios.get('https://anthonygallegos.devcamp.space/portfolio/portfolio_items')
       .then(response => {
-        console.log(response);
+        // console.log("response data",response);
+        this.setState({
+            data: response.data.portfolio_items
+        });
       })
       .catch(error => {
         console.log(error);
       });
     }
 
-    // using the filter function to filter over the data elements that are in the intial state we are able to group the data by its category and render/set a new state of just those items
-    handleFilter(filter) {
-        this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
+    portfolioItems() {
+        return this.state.data.map(item => {
+            console.log("item data", item);
+            return <PortfolioItem key={item.id} title={item.name} url={item.url} slug={item.id}/>// props
+        });
     }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    };
 
     render() {
         if (this.state.isLoading) {
             return <div>Is Loading...</div>
         };
 
-        this.getPortfolioItems();
         return (
             <div>
             <h1>{this.state.pageTitle}</h1>
