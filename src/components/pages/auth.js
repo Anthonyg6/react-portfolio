48e5a2783f831f03fc6a81bd2de245fedd6de801
Anthonyg6 <anthonygallegos6@gmail.com>
2019-04-14 17:04:56 -0600

    added static image to auth page

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import LogoImg from '../../../static/assets/images/auth.jpg';
 
 export default class Auth extends Component {
   render() {
     return (
-      <div>
-        Auth......
+      <div className = "auth-page-wrapper">
+        <div 
+        className ="left-column"
+        style = {{
+          backgroundImage: `url(${LogoImg})`,
+        }}
+        />
+
+
+        <div className = "right-column">
+          <h1>Auth info goes here</h1>
+        </div>
       </div>
-    );
+    )
   }
 }
