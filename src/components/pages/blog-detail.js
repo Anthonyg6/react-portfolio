836f593edc4c6fd0432c8adf539fc7310fe7be88
Anthonyg6 <anthonygallegos6@gmail.com>
2019-05-18 10:14:32 -0600

    pulling in blog data from the api

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class BlogDetail extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentId : this.props.match.params.slug,
+            blogItem: {}
+        };
+    }
+
+    getBlogItem() {
+        Axios.get(`https://anthonygallegos.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+        ).then(response => {
+            console.log("response", response);
+        }).catch(error => {
+            console.log("getBlogItem error", error);
+        });
+    }
+
+    componentDidMount() {
+        this.getBlogItem();
+    }
   render() {
     return (
       <div>
