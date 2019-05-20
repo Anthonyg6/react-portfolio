ca229f7865f32674b59c5e0f8f4e0828b65ea9a5
Anthonyg6 <anthonygallegos6@gmail.com>
2019-05-19 21:10:31 -0600

    added styles to blog details

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,43 +1,54 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
+import React, { Component } from "react";
+import Axios from "axios";
 
 export default class BlogDetail extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            currentId : this.props.match.params.slug,
-            blogItem: {}
-        };
-    }
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    };
+  }
 
-    getBlogItem() {
-        Axios.get(`https://anthonygallegos.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
-        ).then(response => {
-            console.log("response", response);
-            this.setState({
-                blogItem: response.data.portfolio_blog
-            })
-        }).catch(error => {
-            console.log("getBlogItem error", error);
+  getBlogItem() {
+    Axios.get(
+      `https://anthonygallegos.devcamp.space/portfolio/portfolio_blogs/${
+        this.state.currentId
+      }`
+    )
+      .then(response => {
+        console.log("response", response);
+        this.setState({
+          blogItem: response.data.portfolio_blog
         });
-    }
+      })
+      .catch(error => {
+        console.log("getBlogItem error", error);
+      });
+  }
 
-    componentDidMount() {
-        this.getBlogItem();
-    }
+  componentDidMount() {
+    this.getBlogItem();
+  }
   render() {
-      const {
-          title,
-          blog_status,
-          featured_image_url,
-          content
-      } = this.state.blogItem
+    const {
+      title,
+      blog_status,
+      featured_image_url,
+      content
+    } = this.state.blogItem;
     return (
       <div>
-        <h1>{title}</h1>
-        <img src={featured_image_url}/>
-        <div>{content}</div>
+        <div className="blog-container-wrapper">
+          <div className="content-wrapper">
+            <h1>{title}</h1>
+            <div className="featured-image-wrapper">
+              <img src={featured_image_url} />
+            </div>
+            <div className="content">{content}</div>
+          </div>
+        </div>
       </div>
     );
   }
