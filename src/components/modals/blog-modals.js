30e097e3a36df57f681120104a88c5c7b5abd806
Anthonyg6 <anthonygallegos6@gmail.com>
2019-05-26 19:01:54 -0600

    created a blog form with modal

--- src/components/modals/blog-modals.js
+++ src/components/modals/blog-modals.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactModal from "react-modal";
+import BlogForm from "../blog/blog-form";
 
 ReactModal.setAppElement(".app-wrapper");
 //this is the process on how to clear the error message that would affect a user if they were using a screen reader because when the modal was open with out this the screen reader would attempt to look at the data behind the open modal. .setAppElement(el) expects an element as its argument, which is the class of what wraps up the whole project. Which could be found in the index.html page
@@ -32,7 +33,7 @@ export default class BlogModal extends Component {
           }}
           isOpen={this.props.modalIsOpen}
         >
-          <h1>I am in a modal!!</h1>
+          <BlogForm />
         </ReactModal>
       </div>
     );
