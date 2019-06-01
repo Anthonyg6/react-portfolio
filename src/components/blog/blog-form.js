fd9fc50e7c43b2c3b823ad4c4dd058ff5f980d84
Anthonyg6 <anthonygallegos6@gmail.com>
2019-06-01 08:39:55 -0700

    added functionality to update state in modal form for the content

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -28,6 +28,7 @@ export default class BlogForm extends Component {
 
     formData.append("portfolio_blog[title]", this.state.title);
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+    formData.append("portfolio_blog[content]", this.state.content);
 
     return formData;
   }
