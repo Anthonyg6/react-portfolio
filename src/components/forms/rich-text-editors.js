79f61fd6f3ca27c7aa6a051cf0f087e454560617
Anthonyg6 <anthonygallegos6@gmail.com>
2019-06-05 22:03:21 -0600

    converting images into base64 strings in text editor so images could be uploaded

--- src/components/forms/rich-text-editors.js
+++ src/components/forms/rich-text-editors.js
@@ -3,6 +3,7 @@ import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
+import { resolve } from "url";
 
 export default class RichTextEditors extends Component {
   constructor(props) {
@@ -14,10 +15,21 @@ export default class RichTextEditors extends Component {
 
     this.onEditorStateChange = this.onEditorStateChange.bind(this);
     this.uploadFile = this.uploadFile.bind(this);
+    this.getBase64 = this.getBase64.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
+  }
+
+  getBase64(file, callBack) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => callBack(reader.result);
+    reader.onerror = error => {};
   }
 
   uploadFile(file) {
-    console.log("upload file", file);
+    return new Promise((resolve, reject) => {
+      this.getBase64(file, data => resolve({ data: { link: data } }));
+    });
   }
 
   onEditorStateChange(editorState) {
