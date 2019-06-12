1f0760dafa8e699e90a81cc3115effcb4dd62645
Anthonyg6 <anthonygallegos6@gmail.com>
2019-06-11 21:01:15 -0600

    added styles to detail page

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -20,6 +20,7 @@ export default class PortfolioDetail extends Component {
         }`
       )
       .then(response => {
+        console.log("response", response);
         this.setState({
           portfolioItem: response.data.portfolio_item
         });
@@ -42,10 +43,32 @@ export default class PortfolioDetail extends Component {
       thumb_image_url,
       url
     } = this.state.portfolioItem;
+
+    const bannerStyles = {
+      backgroundImage: "url(" + banner_image_url + ")",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center center"
+    };
+
+    const logoStyles = {
+      width: "200px"
+    };
+
     return (
-      <div>
-        <h2>{name}</h2>
-        <p>{description}</p>
+      <div className="portfolio-detail-wrapper">
+        <div className="portfolio-detail-description-wrapper">
+          <div className="description">{description}</div>
+        </div>
+        <div className="banner" style={bannerStyles}>
+          <img src={logo_url} style={logoStyles} />
+        </div>
+
+        <div className="bottom-content-wrapper">
+          <a href="{url}" className="site-link" target="_blank">
+            Visit {name}
+          </a>
+        </div>
       </div>
     );
   }
