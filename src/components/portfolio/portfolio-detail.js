881f7505ac653d909e7319fb100d9048aa4b5ae1
Anthonyg6 <anthonygallegos6@gmail.com>
2019-06-11 19:56:05 -0600

    rendering in portfolio data from api on screen

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
-import PortfolioItem from "./portfolio-items";
 
 export default class PortfolioDetail extends Component {
   constructor(props) {
     super(props);
 
-    this.setState = {
-      portfolioItem: ""
+    this.state = {
+      portfolioItem: {}
     };
 
     this.getPortfolioItem = this.getPortfolioItem.bind(this);
@@ -21,7 +20,9 @@ export default class PortfolioDetail extends Component {
         }`
       )
       .then(response => {
-        console.log("response", response);
+        this.setState({
+          portfolioItem: response.data.portfolio_item
+        });
       })
       .catch(error => {
         console.log("error in getPortfolioItem", error);
@@ -32,9 +33,19 @@ export default class PortfolioDetail extends Component {
     this.getPortfolioItem();
   }
   render() {
+    const {
+      banner_image_url,
+      category,
+      description,
+      logo_url,
+      name,
+      thumb_image_url,
+      url
+    } = this.state.portfolioItem;
     return (
       <div>
-        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+        <h2>{name}</h2>
+        <p>{description}</p>
       </div>
     );
   }
