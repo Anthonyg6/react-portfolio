a1e3b2e00bd09f88096293120a8bf1ea444b9f36
Anthonyg6 <anthonygallegos6@gmail.com>
2019-06-11 19:37:37 -0600

    intial build out for portfolio detail component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import PortfolioItem from "./portfolio-items";
 
-export default function(props) {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  );
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setState = {
+      portfolioItem: ""
+    };
+
+    this.getPortfolioItem = this.getPortfolioItem.bind(this);
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://anthonygallegos.devcamp.space/portfolio/portfolio_items/${
+          this.props.match.params.slug
+        }`
+      )
+      .then(response => {
+        console.log("response", response);
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItem", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      </div>
+    );
+  }
 }
