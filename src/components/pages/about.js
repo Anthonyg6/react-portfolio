951798a8fecc8dd3777235ba6bf4d758733af85a
Anthonyg6 <anthonygallegos6@gmail.com>
2019-06-23 21:23:06 -0600

    added an update to about me page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react";
 import seflie from "../../../static/assets/images/about-me-selfie.jpg";
+import { Link } from "react-router-dom";
 
 export default function() {
   return (
@@ -13,19 +14,16 @@ export default function() {
         }}
       />
       <div className="right-column">
-        "At vero eos et accusamus et iusto odio dignissimos ducimus qui
-        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-        et quas molestias excepturi sint occaecati cupiditate non provident,
-        similique sunt in culpa qui officia deserunt mollitia animi, id est
-        laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
-        distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
-        cumque nihil impedit quo minus id quod maxime placeat facere possimus,
-        omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem
-        quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
-        ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
-        earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-        voluptatibus maiores alias consequatur aut perferendis doloribus
-        asperiores repellat."
+        Hello my name is Anthony! I am currently a Distribution Manager at a
+        reputable 3PL fulfillment warehouse based in Salt Lake City. I am
+        looking to dive deeper into the Tech World and advance my skill set in
+        full stack development! I have always had a curiosity on how things we
+        use every day on our phones/laptops work and that's what has led me to
+        finally pivot and get out of my comfort zone and dive into the
+        web/software development world. I look forward too speaking with you!
+        <Link className="link-to-contact" to="/contact">
+          Click me
+        </Link>
       </div>
     </div>
   );
